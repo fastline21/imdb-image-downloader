658bc945e2c36d8a1e9195e78e389556b4d8c17c
Joshwa Facistol <fastline2123@gmail.com>
2021-10-30 00:41:36 +0800

    Update page structure of imdb

--- app.js
+++ app.js
@@ -6,67 +6,82 @@ const cheerio = require('cheerio');
 const app = express();
 
 app.get('/:id', async (req, res) => {
-    const { id } = req.params;
-    const requestURL = `https://www.imdb.com/title/${id}`;
+	const { id } = req.params;
+	const requestURL = `https://www.imdb.com/title/${id}`;
 
-    let result = {
-        success: false
-    }
+	let result = {
+		success: false,
+	};
 
-    const getIMDBData = async (requestURL) => {
-        try {
-            const pageResult = await rp(requestURL);
-            const $ = cheerio.load(pageResult);
+	const getIMDBData = async (requestURL) => {
+		try {
+			const pageResult = await rp(requestURL);
+			const $ = cheerio.load(pageResult);
 
-            const title = $('div.title_wrapper > h1', pageResult).text().trim();
-            const imgURLSource = $('div.poster > a', pageResult).attr();
+			const title = $('h1.dxSWFG', pageResult).text().trim();
+			const year = $('a.rgaOW', pageResult)[0].children[0].data.trim();
+			const imgURLSource = $('div.kPdBKI > a', pageResult).attr();
 
-            // Sometimes the poster is undefined
-            // So it needs to return false
-            // In order to rerun the function again.
-            if (!imgURLSource) {
-                return {
-                    success: false
-                }
-            }
+			// Sometimes the poster is undefined
+			// So it needs to return false
+			// In order to rerun the function again.
+			if (!imgURLSource) {
+				return {
+					success: false,
+				};
+			}
 
-            let imgURL = '';
+			let imgURL = '';
 
-            try {
-                const imgResult = await rp(`https://www.imdb.com${imgURLSource.href}`);
-                const $ = cheerio.load(imgResult);
+			try {
+				const imgResult = await rp(
+					`https://www.imdb.com${imgURLSource.href}`
+				);
+				const $ = cheerio.load(imgResult);
 
-                const imgLoad = $('div.ipc-page-content-container > div.media-viewer > div.iUyzNI > img', imgResult);
+				const imgLoad = $(
+					'div.ipc-page-content-container > div.media-viewer > div.iUyzNI > img',
+					imgResult
+				);
 
-                for (let i = 0; i < imgLoad.length; i++) {
-                    const classAttr = imgLoad[i].attribs.class;
-                    if (!classAttr.includes('peek')) {
-                        imgURL = imgLoad[i].attribs.src;
-                    }
-                }
-            } catch (error) {
-                console.log(error);
-                res.status(404).json({ message: error.message });
-            }
-            return {
-                success: true,
-                title,
-                imgURL
-            }
-        } catch (error) {
-            console.log(error);
-            res.status(404).json({ message: error.message })
-        }
-    }
+				for (let i = 0; i < imgLoad.length; i++) {
+					const classAttr = imgLoad[i].attribs.class;
+					if (!classAttr.includes('peek')) {
+						imgURL = imgLoad[i].attribs.src;
+					}
+				}
+			} catch (error) {
+				console.error(error);
+				res.status(404).json({
+					message: error.message,
+				});
+			}
 
-    while (!result.success) {
-        result = { ...await getIMDBData(requestURL) };
-    }
+			return {
+				success: true,
+				title,
+				year,
+				imgURL,
+			};
+		} catch (error) {
+			console.error(error);
+			res.status(404).json({
+				message: error.message,
+			});
+		}
+	};
 
-    res.json({
-        title: result.title,
-        imgURL: result.imgURL
-    });
+	while (!result.success) {
+		result = {
+			...(await getIMDBData(requestURL)),
+		};
+	}
+
+	res.json({
+		title: result.title,
+		year: result.year,
+		imgURL: result.imgURL,
+	});
 });
 
 const port = process.env.PORT || 5000;
